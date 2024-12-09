ed3aabf8b5ff4356110dc65a9583770a47c37969
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-12-10 02:04:00 +0300

    FIRST TRIAL

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.chocolate", NULL, "M", NULL, NULL, 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", NULL, "M", NULL, NULL, 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", NULL, "F", NULL, NULL, 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", NULL, "M", NULL, NULL, 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", NULL, "F", NULL, NULL, 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", NULL, "M", NULL, NULL, 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", NULL, "F", NULL, NULL, 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, 1);
